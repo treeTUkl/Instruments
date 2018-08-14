fe210dfcbcc73e63b980fd79b214327d87facbec
larpid <pidde@rhrk.uni-kl.de>
2018-08-14 16:59:38 +0200

    automated get min and max position of stage

--- PIStage.py
+++ PIStage.py
@@ -22,7 +22,7 @@ class PIStage(Stage.Stage):
         self.position_min = 0  # gets set when stage connects
         #        self.position_in_ps = 2 * 3.33333 * self.path * self.position_current
         #        self.configuration = {'zero position': 0}
-        self.velocity = 10  # unit: mm/s
+        self.velocity = 1  # unit: mm/s
         self.ser = serial.Serial()
         print('init succ')
 
@@ -30,28 +30,69 @@ class PIStage(Stage.Stage):
         self.ser = serial.Serial(
             port='/dev/ttyUSB0',  # todo: make not hardcoded
             baudrate=57600,
-            timeout=3,
+            timeout=2,
             bytesize=8,
             parity='N',
             stopbits=1)  # open serial port
         # baudrate, timeout, bytesize, parity and stopbits values copied from PITerminal standard values
-        print(self.ser.name)  # check which port is really used
-        self.ser.write('IDN?\n')
-        print(self.ser.readlines())
+        #print(self.ser.name)  # check which port is really used
+        #
+        # get minimum position:
         self.ser.write('TMN?\n')
         outs = self.ser.readlines()
-        self.position_min = parse("{}={}", outs[0])[1]
-        print(outs[1])
+        self.position_min = parse('{}={:e} \n', outs[0])[1]
+        print('position_min is now set to: '+str(self.position_min))
+        self.pi_error_check()
+        #
+        # get maximum position:
         self.ser.write('TMX?\n')
-        out1 = self.ser.readline()
-        out2 = self.ser.readline()
-        self.position_max = parse("{}={}", out1)[1]
-        print(out2)
+        outs = self.ser.readlines()
+        self.position_max = parse('{}={:e} \n', outs[0])[1]
+        print('position_max is now set to: ' + str(self.position_max))
+        self.pi_error_check()
+        #
+        #
         self.ser.write('POS?\n')
         outs = self.ser.readlines()
         for eachLine in outs:
-            print(eachLine)
+            print('position: '+eachLine)
+        self.pi_error_check()
+        #
+        # set velocity
+        self.ser.write('VEL 1 '+str(self.velocity)+'\n')
+        self.ser.write('VEL?\n')
+        print('velocities:')
+        print(self.ser.readlines())
+        self.pi_error_check()
+        #
+        # set servo
+        self.ser.write('SVO 1 1\n')
+        self.ser.write('SVO?\n')
+        print('servo state:')
+        print(self.ser.readlines())
+        self.pi_error_check()
+        self.ser.write('FRF 1\n')
+        self.pi_error_check()
+        self.ser.write('FRF?\n')
+        print('FRF result:')
+        print(self.ser.readlines())
+        self.pi_error_check()
+        self.ser.write('MOV 1 5\n')
 
     def disconnect(self):
         self.ser.close()
         print('Stage has been disconnected')
+
+    def move_absolute(self,newPos):
+        self.ser.write('MOV 1 '+str(newPos)+'\n')
+
+    def PI_GetControllerIdentification(selfself):
+        self.ser.write('IDN?\n')
+        for eachline in self.ser.readlines():
+            print(eachline)
+
+    def pi_error_check(self, force_output=False):
+        self.ser.write('ERR?\n')
+        readbuffer=self.ser.readlines()
+        if (readbuffer[0][0]!='0' or force_output):
+            print('Controller reports Error Code: '+readbuffer[0])

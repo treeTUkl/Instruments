e0a57fc439e2ac9b66e4ebc5cc4ac57b06678bcc
larpid <pidde@rhrk.uni-kl.de>
2018-08-15 18:08:55 +0200

    PIStage.py now does most of its recommended magic, PItest.py runs a few move commands. Next step would be to handle implementation for multiple axes (as they are not recognized by Stage.py) and create a pyTango server using PIStage.py

--- PIStage.py
+++ PIStage.py
@@ -12,64 +12,71 @@ import Stage
 import time
 import serial
 from parse import *
+from serial.tools.list_ports import comports
 
 
 class PIStage(Stage.Stage):
 
     def __init__(self):
-        #        self.path = 1
-        self.position_max = 0  # gets set when stage connects
-        self.position_min = 0  # gets set when stage connects
+        self.position_max = {}  # gets set per axis when stage connects
+        self.position_min = {}  # gets set per axis when stage connects
         #        self.position_in_ps = 2 * 3.33333 * self.path * self.position_current
         #        self.configuration = {'zero position': 0}
-        self.velocity = 1  # unit: mm/s
+        self.velocity = {'1': 2, '2': 1}  # unit: mm/s
         self.ser = serial.Serial()
-        print('init succ')
 
     def connect(self):
+        ports_available = comports()
+        print(ports_available)
+        print('select device:')
+        for portindex, eachport in enumerate(ports_available):
+            print(str(portindex)+' - '+eachport.device+'\t'+eachport.description)
+        port=ports_available[input()].device
         self.ser = serial.Serial(
-            port='/dev/ttyUSB0',  # todo: make not hardcoded
+            port=port,
             baudrate=57600,
-            timeout=2,
+            # baudrate, timeout, bytesize, parity and stopbits values copied from PITerminal standard values
+            timeout=1,
             bytesize=8,
             parity='N',
             stopbits=1)  # open serial port
-        # baudrate, timeout, bytesize, parity and stopbits values copied from PITerminal standard values
-        #print(self.ser.name)  # check which port is really used
         #
-        # get minimum position:
+        # confirm connection:
+        print('connection established, stage respond on command IDN?:')
+        self.ser.write('IDN?\n')
+        print(self.ser.readlines())
+        #
+        # get minimum positions:
         self.ser.write('TMN?\n')
-        outs = self.ser.readlines()
-        self.position_min = parse('{}={:e} \n', outs[0])[1]
-        print('position_min is now set to: '+str(self.position_min))
+        readbuffer = self.ser.readlines()
+        for eachline in readbuffer:
+            self.position_min[parse('{}={:e}{}', eachline)[0]] = parse('{}={:e}{}', eachline)[1]
+        print('position_min is now set to: ' + str(self.position_min))
         self.pi_error_check()
         #
-        # get maximum position:
+        # get maximum positions:
         self.ser.write('TMX?\n')
-        outs = self.ser.readlines()
-        self.position_max = parse('{}={:e} \n', outs[0])[1]
+        readbuffer = self.ser.readlines()
+        for eachline in readbuffer:
+            self.position_max[parse('{}={:e}{}', eachline)[0]] = parse('{}={:e}{}', eachline)[1]
         print('position_max is now set to: ' + str(self.position_max))
         self.pi_error_check()
         #
-        #
+        # TODO: use this for sth or remove
         self.ser.write('POS?\n')
-        outs = self.ser.readlines()
-        for eachLine in outs:
-            print('position: '+eachLine)
+        print('positions:')
+        print(self.ser.readlines())
         self.pi_error_check()
         #
-        # set velocity
-        self.ser.write('VEL 1 '+str(self.velocity)+'\n')
+        # set velocities
+        for axis_id in self.velocity:
+            self.ser.write('VEL '+axis_id+' '+str(self.velocity[axis_id])+'\n')
         self.ser.write('VEL?\n')
         print('velocities:')
         print(self.ser.readlines())
         self.pi_error_check()
         #
-        # set servo
-        self.ser.write('SVO 1 1\n')
-        self.ser.write('SVO?\n')
-        print('servo state:')
-        print(self.ser.readlines())
+        # FRF
         self.pi_error_check()
         self.ser.write('FRF 1\n')
         self.pi_error_check()
@@ -77,22 +84,33 @@ class PIStage(Stage.Stage):
         print('FRF result:')
         print(self.ser.readlines())
         self.pi_error_check()
-        self.ser.write('MOV 1 5\n')
 
     def disconnect(self):
         self.ser.close()
-        print('Stage has been disconnected')
+        print('Connection has been closed')
 
-    def move_absolute(self,newPos):
-        self.ser.write('MOV 1 '+str(newPos)+'\n')
+    def move_absolute(self, axisID, newPos):
+        if (newPos > self.position_min[axisID] and newPos < self.position_max[axisID]):
+            self.PI_servo(axisID)
+            self.ser.write('MOV '+axisID+' ' + str(newPos) + '\n')
+        else:
+            print('no')
 
     def PI_GetControllerIdentification(selfself):
         self.ser.write('IDN?\n')
         for eachline in self.ser.readlines():
             print(eachline)
 
+    def PI_servo(self,axisID):
+        self.ser.write('SVO '+axisID+' 1\n')
+        self.ser.write('SVO? '+axisID+'\n')
+        print('servo state:')
+        print(self.ser.readline())
+
     def pi_error_check(self, force_output=False):
         self.ser.write('ERR?\n')
-        readbuffer=self.ser.readlines()
-        if (readbuffer[0][0]!='0' or force_output):
-            print('Controller reports Error Code: '+readbuffer[0])
+        readbuffer = self.ser.readlines()
+        if (readbuffer[0][0] != '0' or force_output):
+            print('Controller reports Error Code: ' + readbuffer[0])
+        if readbuffer[0][0] != '0':
+            exit()

53f5e37dd7c2f28f115ed27d3bbe52843b0c1b14
larpid <pidde@rhrk.uni-kl.de>
2018-10-11 16:12:13 +0200

    restructured some functions

--- PIStage.py
+++ PIStage.py
@@ -9,50 +9,59 @@ import Stage
 import sys
 import time
 import serial
-from parse import *
 from serial.tools import list_ports
 
 
 class PIStage(Stage.Stage):
 
-    def __init__(self, axis='1', velocity=2):
+    def __init__(self, controller_serial_number, axis='1', velocity=2):
         super(PIStage, self).__init__()
         self.axis = axis
+        self.controller_serial_number = controller_serial_number
         self.velocity = velocity  # unit: mm/s
         self.ser = serial.Serial()
 
-    def connect(self, controller_serial_number):
-        self.PI_connect(controller_serial_number)
-        self.PI_setup()
+    def connect(self):
+        self.pi_connect()
+        self.pi_handle_limits()
+        self.pi_set_velocity()
+        self.pi_zero_reference_move()
 
+    def disconnect(self):
+        self.ser.close()
+        print('Connection has been closed')
 
-    def selectStagePort(self):
-        ports_available = list_ports.comports()
-        print(ports_available)
-        print('select device:')
-        for portindex, eachport in enumerate(ports_available):
-            print(str(portindex) + ' - ' + eachport.device + '\t' + eachport.description)
-        return ports_available[int(input())].device
+    def move_absolute(self, new_position):
+        """Move the stage to the given position in its range"""
 
-    def PI_connect(self, controller_serial_number):
-        '''connect to PI stage and confirm serial number'''
-        print(list_ports.comports())
+        time_to_sleep = (abs(self.position_current - new_position)) / self.velocity
+        if self.position_min <= new_position <= self.position_max:
+            self.pi_servo()
+            self.ser.write('MOV ' + self.axis + ' ' + str(new_position) + '\n')
+            self.position_current = new_position
+            time.sleep(time_to_sleep)
+            print('Stage was moved to ' + str(new_position))
+        else:
+            print('position is out of range')
+
+    def position_get(self):
+        """return stage position"""
+
+        for answer_line in self.pi_request('POS?\n'):
+            if answer_line[:len(self.axis)] == self.axis:
+                return answer_line[len(self.axis) + 1:]
+
+    def pi_connect(self):
+        """connect to PI stage controller and confirm serial number"""
+
         for each_port in list_ports.comports():
             if each_port.manufacturer == 'PI':
-                self.ser = serial.Serial(   # baudrate, timeout, bytesize, parity and stopbits values copied from PITerminal standard values
-                    port=each_port.device,
-                    baudrate=57600,
-                    timeout=1,
-                    bytesize=8,
-                    parity='N',
-                    stopbits=1)
+                self.ser = serial.Serial(
+                    port=each_port.device, baudrate=57600, timeout=1, bytesize=8, parity='N', stopbits=1)
                 print('test connection established to device:' + each_port.device)
-                # test connection:
-                self.ser.write('IDN?\n')
-                response_idn = self.PI_read()
-                current_controller_serial_number = response_idn[0].split(',')[2]
-                # int conversion in if branch is to get rid of leading zero
-                if int(current_controller_serial_number) == int(controller_serial_number):
+                response_idn = self.pi_request('IDN?\n')
+                if int(response_idn[0].split(',')[2]) == int(self.controller_serial_number):
+                    # int conversion is to get rid of leading zero
                     print('connection established, stage respond on command \"IDN?\":')
                     print(response_idn)
                     return
@@ -61,88 +70,61 @@ class PIStage(Stage.Stage):
         print('connection failed:\n' +
               'no controller with serial number ' + controller_serial_number + ' found.')
 
-    def PI_setup(self): #needs reworking
-        # get minimum positions:
-        self.ser.write('TMN?\n')
-        readbuffer = self.PI_read()
-        for eachline in readbuffer:
-            if parse('{}={:e}', eachline)[0] == self.axis:
-                self.position_min = parse('{}={:e}', eachline)[1]
+    def pi_handle_limits(self):
+        """save minimum and maximum position"""
+        # set minimum positions:
+        for line in self.pi_request('TMN?\n'):
+            if line.split('=')[0] == self.axis:
+                self.position_min = float(line.split('=')[1])
         print('position_min is now set to: ' + str(self.position_min))
         self.pi_error_check()
         #
         # get maximum positions:
-        self.ser.write('TMX?\n')
-        readbuffer = self.PI_read()
-        for eachline in readbuffer:
-            if parse('{}={:e}', eachline)[0] == self.axis:
-                self.position_max = parse('{}={:e}', eachline)[1]
+        for line in self.pi_request('TMX?\n'):
+            if line.split('=')[0] == self.axis:
+                self.position_max = float(line.split('=')[1])
         print('position_max is now set to: ' + str(self.position_max))
         self.pi_error_check()
-        #
-        # TODO: use this for sth or remove
-        self.ser.write('POS?\n')
-        print('positions:')
-        print(self.PI_read())
-        self.pi_error_check()
-        #
-        # set velocity
+
+    def pi_set_velocity(self):
         self.ser.write('VEL ' + self.axis + ' ' + str(self.velocity) + '\n')
-        self.ser.write('VEL?\n')
         print('velocities:')
-        print(self.PI_read())
+        print(self.pi_request('VEL?\n'))
         self.pi_error_check()
-        #
-        # FRF
+
+    def pi_zero_reference_move(self):
         self.pi_error_check()
         self.ser.write('FRF 1\n')
+        time.sleep(5)  # this leaves room for optimization
         self.pi_error_check()
-        self.ser.write('FRF?\n')
         print('FRF result:')
-        print(self.PI_read())
+        print(self.pi_request('FRF?\n'))
         self.pi_error_check()
 
-    def disconnect(self):
-        self.ser.close()
-        print('Connection has been closed')
+    def pi_request(self, request_command):
+        """request controller respond"""
 
-    def move_absolute(self, new_position):
-        '''Move the stage to the given position in its range'''
-
-        time_to_sleep = (abs(self.position_current - new_position)) / self.velocity
-        if new_position >= self.position_min and new_position <= self.position_max:
-            self.PI_servo(self.axis)
-            self.ser.write('MOV ' + self.axis + ' ' + str(new_position) + '\n')
-            self.position_current = new_position
-            time.sleep(time_to_sleep)
-            print('Stage was moved to ' + str(new_position))
-        else:
-            print('position is out of range')
-
-    def PI_GetControllerIdentification(selfself):
-        self.ser.write('IDN?\n')
-        for eachline in self.PI_read():
-            print(eachline)
-
-    def PI_read(self):
-        '''read stage respond'''
+        self.ser.reset_input_buffer()
+        self.ser.write(request_command)
         line_current = self.ser.read_until('\n')
         lines_read = [line_current.strip()]
-        while line_current[-2]==' ':
+        while line_current[-2] == ' ':
             line_current = self.ser.read_until('\n')
             lines_read.append(line_current.strip())
         return lines_read
 
-    def PI_servo(self, axisID):
-        self.ser.write('SVO ' + axisID + ' 1\n')
-        self.ser.write('SVO? ' + axisID + '\n')
+    def pi_servo(self):
+        """check servo state"""
+
+        self.ser.write('SVO ' + self.axis + ' 1\n')
         print('servo state:')
-        print(self.ser.readline())
+        print(self.pi_request('SVO? ' + self.axis + '\n'))
 
     def pi_error_check(self, force_output=False):
-        self.ser.write('ERR?\n')
-        readbuffer = self.PI_read()
-        if (readbuffer[0][0] != '0' or force_output):
-            print('Controller reports Error Code: ' + readbuffer[0])
-        if readbuffer[0][0] != '0':
+        """request controller errors"""
+
+        err_answer = self.pi_request('ERR?\n')
+        if err_answer[0][0] != '0' or force_output:
+            print('Controller reports Error Code: ' + err_answer[0])
+        if err_answer[0][0] != '0':
             exit()
--- PItest.py
+++ PItest.py
@@ -1,12 +1,18 @@
 import PIStage
-import time
 
-c413=PIStage.PIStage()
+c413 = PIStage.PIStage('117018374')
 
-c413.connect('117018374')
+c413.connect()
+c413.pi_error_check(True)
 c413.move_absolute(-4)
+print(c413.position_get())
 c413.move_absolute(2)
+print(c413.position_get())
+c413.pi_error_check(True)
 c413.move_absolute(-9)
+print(c413.position_get())
 c413.move_absolute(-40)
 c413.move_absolute(9)
+print(c413.position_get())
+c413.pi_error_check(True)
 c413.disconnect()

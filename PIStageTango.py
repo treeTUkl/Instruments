d41bf89f1feb8823ebe52e8f661aaf77f1a4a3c0
larpid <pidde@rhrk.uni-kl.de>
2018-11-22 16:17:06 +0100

    started implementation of CmdTrig attributes because cs studio does not seem  to support Tangos command system

--- PIStageTango.py
+++ PIStageTango.py
@@ -1,12 +1,10 @@
 import PIStage
 from tango import AttrQuality, AttrWriteType, DispLevel
-from PyTango import DevState
+from PyTango import DevState, DebugIt
 from PyTango.server import run
 from PyTango.server import Device, DeviceMeta
 from PyTango.server import attribute, command, pipe
 from PyTango.server import class_property, device_property
-
-
 
 
 class PIStageTango(Device):  # , metaclass=DeviceMeta): (way to go on python3 instead of next line)
@@ -14,25 +12,22 @@ class PIStageTango(Device):  # , metaclass=DeviceMeta): (way to go on python3 in
 
     controller_serial_number = '117018374'
     stage = PIStage.PIStage(controller_serial_number=controller_serial_number)
-
-    position = attribute(label="Position", dtype=float,
-                        display_level=DispLevel.EXPERT,
-                        access=AttrWriteType.READ,
-                        unit="mm",
-                        min_value=stage.position_min, max_value=stage.position_max,
-                        fget="position_get",
-                        doc="stage position")
+    stage.connect()
 
     def init_device(self):
         Device.init_device(self)
-        self.stage.connect()
         self.set_state(DevState.ON)
 
-    def position_get(self):
-        return self.stage.position_get()
+    position = attribute(label="Position", dtype=float,
+                        display_level=DispLevel.EXPERT,
+                        access=AttrWriteType.READ_WRITE,
+                        unit="mm",
+                        min_value=stage.position_min, max_value=stage.position_max,
+                        fget="read_position",
+                        fset="Move_absolute",
+                        doc="stage position")
 
-    @attribute
-    def position_get2(self):
+    def read_position(self):
         return self.stage.position_get()
 
     @command(dtype_in=float)
@@ -41,6 +36,13 @@ class PIStageTango(Device):  # , metaclass=DeviceMeta): (way to go on python3 in
         self.stage.move_absolute(new_pos)
         self.set_state(DevState.ON)
         print('moved stage...TANGO to: ' + str(new_pos))
+
+    CmdTrig_zero_reference_move = attribute(access=AttrWriteType.WRITE,
+                                            fset="set_CmdTrig_zero_reference_move")
+
+    def set_CmdTrig_zero_reference_move(self, value):
+        print(value)
+        self.Zero_reference_move()
 
     @command
     def Zero_reference_move(self):
@@ -52,7 +54,6 @@ class PIStageTango(Device):  # , metaclass=DeviceMeta): (way to go on python3 in
                 dict(manufacturer='PI',
                      model='C-413.2GA',
                      serial_number=self.controller_serial_number))
-
 
 if __name__ == "__main__":
     run([PIStageTango])
